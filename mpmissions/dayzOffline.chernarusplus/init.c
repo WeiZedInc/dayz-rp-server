3d9d1d65868aa108fa228d3f097720427c8bd4a8
Holdazar <98821391+Holdazar@users.noreply.github.com>
2022-02-01 21:23:45 +0200

    Drop trash & Update Init.c

--- mpmissions/dayzOffline.chernarusplus/init.c
+++ mpmissions/dayzOffline.chernarusplus/init.c
@@ -1,9 +1,9 @@
-#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\CustomMission.c"
-#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\scripts\\Economy.c"
-#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\scripts\\\CustomSpawnPlayerConfig\\init_mod.c"
-
 void main()
 {
-	InitEconomy();
+	/*
+	 * Weized С-СЮКА НАУЧИСЬ НОРМАЛЬ КОМИТИТЬ (╬▔皿▔)╯
+	 *
+	 * Падлюка! (ﾟДﾟ*)ﾉ
+	*/
 }
 
