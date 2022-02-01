53d656dda000664193b9170a1620d3929ccd100f
WeiZedInc <weizedinc@gmail.com>
2022-02-01 21:12:29 +0200

    bat

--- mpmissions/dayzOffline.chernarusplus/init.c
+++ mpmissions/dayzOffline.chernarusplus/init.c
@@ -1,7 +1,9 @@
-#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\scripts\\razrabovskazki.c"
+#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\CustomMission.c"
+#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\scripts\\Economy.c"
+#include "$CurrentDir:\\mpmissions\\dayzOffline.chernarusplus\\scripts\\\CustomSpawnPlayerConfig\\init_mod.c"
 
 void main()
 {
-	Init_razrabovskazki();
+	InitEconomy();
 }
 
